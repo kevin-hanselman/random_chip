8d80d4ce5bb2977df63e26b1f69f4701a52b3f94
Kevin Hanselman <kevin.hanselman@gmail.com>
2016-12-26 18:12:50 -0500

    fix bug

--- install_si7021.sh
+++ install_si7021.sh
@@ -7,4 +7,4 @@ gcc -Wall -Werror si7021.c -o ~/si7021.bin
 # create the crontab if necessary so first crontab -l doesn't error-out
 [ -n "$(crontab -l 2>/dev/null)" ] || printf '' | crontab -
 
-(crontab -l | grep -v 'si7021' ; echo "*/2 * * * * $HOME/si7021.bin >> $HOME/si7021.log") | crontab -
+(crontab -l | grep -v 'si7021' || true; echo "*/2 * * * * $HOME/si7021.bin >> $HOME/si7021.log") | crontab -
